2ececa393e026d9afde943673ec4ff6f08756a06
vedantmahawar <vedantmahawar@outlook.com>
2025-12-29 18:57:43 -0800

    add Technology section

--- app/page.tsx
+++ app/page.tsx
@@ -23,12 +23,8 @@ import {
 
 // icons
 import {
-  Box,
-  Flame,
-  ArrowLeftRight,
-  BrickWall,
-  ArrowRight,
-  Mail,
+  CircleDollarSign,
+  ChartNoAxesCombined
 } from "lucide-react";
 
 // data
@@ -44,35 +40,22 @@ export default function Home() {
           Humanoids for the <br /> price of an iPhone
         </h1>
         <h2 className="text-lg sm:text-2xl font-medium mb-4 sm:mb-6">
-          We've built a full-size, mass-producible <br />
-          humanoid robot in 2 months for &lt;$1000
+          Built to scale from day one
         </h2>
       </div>
       {/* Our Belief */}
       <div className="w-full max-w-6xl mx-auto px-8 xl:px-0 mb-20 sm:mb-40 text-balance text-center">
         <h4 className="text-lg sm:text-normal font-medium mb-2 text-blue-500">
-          Our Vision
+          Our Belief
         </h4>
         <h2 className="text-4xl sm:text-5xl font-medium mb-2">
           Mass production before autonomy
         </h2>
         <h3 className="text-lg sm:text-xl font-normal mb-10 text-neutral-500">
-          To achieve autonomy you need a boatload of data, yet existing humanoid
-          companies can only produce a few hundred units per year.
-        </h3>
-      </div>
-      {/* The Technology (muscles section) */}
-      <div className="w-full max-w-6xl mx-auto px-8 xl:px-0 mb-20 sm:mb-40 text-balance text-left">
-        <h4 className="text-lg sm:text-normal font-medium mb-2 text-blue-500">
-          Our Technology
-        </h4>
-        <h2 className="text-4xl sm:text-5xl font-medium mb-2">
-          Proprietary artificial muscles
-        </h2>
-        <h3 className="text-lg sm:text-xl font-normal mb-10 text-neutral-500">
-          Instead of following the usual playbook (raise $100M and spend years
-          on actuators), we've designed our humanoid around artificial muscles
-          from day one.
+          Tesla required billions of miles of driving data from millions of
+          vehicles worldwide to enable self driving with just two degrees of
+          freedom. Humanoids have &gt;50 degrees of freedom, meaning they
+          require much more data to reach autonomy.
         </h3>
       </div>
       {/* Cost */}
@@ -84,8 +67,44 @@ export default function Home() {
           As cheap as an iPhone
         </h2>
         <h3 className="text-lg sm:text-xl font-normal mb-10 text-neutral-500">
-          A household robot cannot be a 70kg, $20,000 metal beast.
+          To achieve mass production we've built a robot for &lt;$1,000 dollars.
         </h3>
+      </div>
+      {/* The Technology (muscles section) */}
+      <div className="w-full max-w-6xl mx-auto px-8 xl:px-0 mb-20 sm:mb-40 text-balance text-left">
+        <h4 className="text-lg sm:text-normal font-medium mb-2 text-blue-500">
+          Technology
+        </h4>
+        <h2 className="text-4xl sm:text-5xl font-medium mb-2">
+          Proprietary artificial muscles
+        </h2>
+        <h3 className="text-lg sm:text-xl font-normal mb-10 text-neutral-500">
+          Our humanoid is so cheap because we've designed it around low-cost artificial muscles
+          from day one.
+        </h3>
+        <div className="flex flex-row gap-10">
+          <div className="border rounded-md p-4 shadow-xl">
+            <CircleDollarSign className="text-blue-500 mb-2" />
+              <h3 className="text-xl sm:text-2xl font-medium">Low-cost</h3>
+              <p className="text-md sm:text-lg text-neutral-500">
+                Unlike competitors who use electric actuators our artificial
+                muscles are much cheaper, and more human
+              </p>
+          </div>
+          <div className="border rounded-md p-4 shadow-xl">
+           <ChartNoAxesCombined className="text-blue-500 mb-2" />
+            <h3 className="text-xl sm:text-2xl font-medium">
+              Easily mass producible
+            </h3>
+            <p className="text-md sm:text-lg text-neutral-500">
+              Each muscle is just a tube wrapped in braided fibre, and the whole robot is powered by a single pump
+            </p>
+          </div>
+                      {/* designed to scale a graph of exponetial growth */}
+            {/* globe,  */}
+        </div>
+        {/* hip actuator for tesla is 200 dollars alone */}
+
       </div>
       {/* Our Competitors */}
       <div className="w-full max-w-6xl mx-auto px-8 xl:px-0 mb-20 sm:mb-40 text-balance text-center">
@@ -98,7 +117,7 @@ export default function Home() {
         <h3 className="text-lg sm:text-xl font-normal mb-10 text-neutral-500">
           Only capable of manufacturing a few hundred humanoids per year.
         </h3>
-        <ComparsionTable data={comparisonData}/>
+        <ComparsionTable data={comparisonData} />
       </div>
       {/* Use Cases */}
       <div className="w-full max-w-6xl mx-auto px-8 xl:px-0 mb-20 sm:mb-40 text-balance text-left">
@@ -109,7 +128,7 @@ export default function Home() {
           Never do your chores again
         </h2>
         <h3 className="text-lg sm:text-xl font-normal mb-10 text-neutral-500">
-          Only capable of manufacturing a few hundred humanoids per year.
+          Only capable of manufacturing a few hundred humanoids per year.  A household robot cannot be a 70kg, $20,000 metal beast.
         </h3>
       </div>
       {/* Somewhere mention that it does your chores? mayeb give examples earlier, playing tennis picking balls?)*/}
@@ -127,46 +146,7 @@ export default function Home() {
           engineers & global robotics competition champions.
         </h3>
       </div>
-      {/* Final CTA -> helps us envision a world where no one does chores ( -> get in contact */}
-
-      <div className="w-full max-w-6xl mx-auto px-8 xl:px-0 mb-20 sm:mb-40 text-balance">
-        <div className="grid grid-rows-4 sm:grid-rows-2 sm:grid-cols-2 border border-dashed">
-          <div className="p-4 sm:border-r border-b border-dashed">
-            <Box color="#f87171" className="mb-2" />
-            <h3 className="text-xl sm:text-2xl font-medium">Low-cost</h3>
-            <p className="text-md sm:text-lg text-neutral-500">
-              Unlike competitors who use electric actuators our artificial
-              muscles are much cheaper, and more human
-            </p>
-          </div>
-          <div className="p-4 border-b border-dashed">
-            <Flame color="#f87171" className="mb-2" />
-            <h3 className="text-xl sm:text-2xl font-medium">22 DOF</h3>
-            <p className="text-md sm:text-lg text-neutral-500">
-              Designed for high-performance materials, so you never have to
-              compromise
-            </p>
-          </div>
-          <div className="p-4 border-b sm:border-b-0 sm:border-r border-dashed">
-            <BrickWall color="#f87171" className="mb-2" />
-            <h3 className="text-xl sm:text-2xl font-medium">
-              1.5m tall build chamber
-            </h3>
-            <p className="text-md sm:text-lg text-neutral-500">
-              Never worry about a part being too large
-            </p>
-          </div>
-          <div className="p-4">
-            <ArrowLeftRight color="#f87171" className="mb-2" />
-            <h3 className="text-xl sm:text-2xl font-medium">
-              Switchable end effectors
-            </h3>
-            <p className="text-md sm:text-lg text-neutral-500">
-              Switch between additive and subtractive manufacturing with ease
-            </p>
-          </div>
-        </div>
-      </div>
+      {/* Final CTA -> the most capital-efficient humanoid company helps us envision a world where no one does chores ( -> get in contact */}
     </div>
   );
 }
