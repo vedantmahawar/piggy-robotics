7cd4e5868367293765ab26a53dd8d0bd244b7159
vedantmahawar <vedantmahawar@outlook.com>
2025-12-30 13:40:00 -0800

    update Footer

--- components/custom/Footer.tsx
+++ components/custom/Footer.tsx
@@ -1,14 +1,18 @@
 // custom components
 import ExternalLink from "@/components/custom/ExternalLink";
 
+// icons
+import { Linkedin } from "lucide-react";
+import { SiX } from "@icons-pack/react-simple-icons";
+
 // Next
 import Link from "next/link";
 import Image from "next/image";
 
 export default function Footer() {
   return (
-    <div className="">
-      <div className="w-full mx-auto max-w-6xl flex justify-between border-t border-neutral-700 py-5 px-5 xl:px-0">
+    <div className="overflow-hidden">
+      <div className="w-full mx-auto max-w-6xl flex flex-col sm:flex-row justify-between border-t border-neutral-400 py-5 px-5 xl:px-0">
         <Link href="/">
           <Image
             src="/logo.png"
@@ -18,16 +22,20 @@ export default function Footer() {
             className="w-16 sm:w-20"
           />
         </Link>
-        <div className="flex flex-row gap-20">
-          <div>
-            <p className="font-semibold mb-1">Company</p>
-            <Link href="/our-team">
-              <p>Our Team</p>
-            </Link>
-          </div>
-        </div>
       </div>
-      <div className="w-full mx-auto max-w-6xl flex justify-between py-5 px-5 xl:px-0">
+      <div className="relative w-full mx-auto max-w-6xl flex justify-between py-5 px-5 xl:px-0 items-center">
+        <div
+          className="absolute -bottom-15 right-0 h-30 w-96 -z-10 
+        bg-radial from-orange-300 to-transparent blur-3xl"
+        />
+        <div
+          className="absolute -bottom-15 right-48 h-30 w-96 -z-10 
+        bg-radial from-red-300 to-transparent blur-3xl"
+        />
+        <div
+          className="absolute -bottom-15 right-96 h-30 w-96 -z-10 
+        bg-radial from-sky-300 to-transparent blur-3xl"
+        />
         <p>
           © 2025 Piggy Robotics. <br className="sm:hidden" />
           Designed by{" "}
@@ -36,6 +44,17 @@ export default function Footer() {
           </ExternalLink>
           .
         </p>
+        <div className="flex flex-row gap-3">
+          <ExternalLink
+            link="https://www.linkedin.com/company/piggy-robotics/"
+            color="nocolor"
+          >
+            <Linkedin size={20} />
+          </ExternalLink>
+          <ExternalLink link="https://x.com/PiggyRobotics" color="nocolor">
+            <SiX size={20} />
+          </ExternalLink>
+        </div>
       </div>
     </div>
   );
